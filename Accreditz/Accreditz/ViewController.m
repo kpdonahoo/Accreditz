0eb231288511755d2ce46c93c713d4a195642548
Amanda Doyle <Amanda@Amandas-MacBook-Pro-2.local>
2014-11-17 22:55:47 -0600

    font update

--- Accreditz/Accreditz/ViewController.m
+++ Accreditz/Accreditz/ViewController.m
@@ -11,16 +11,18 @@
 @interface ViewController ()
 @property (strong, nonatomic) IBOutlet UIView *connectButton;
 @property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@property (weak, nonatomic) IBOutlet UIButton *button;
 
 @end
 
-//meep
-
+//Demonstration
 @implementation ViewController
 @synthesize resultLabel;
+@synthesize button;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    button.font = [UIFont fontWithName:@"ElephantsinCherryTrees" size:20];
     // Do any additional setup after loading the view, typically from a nib.
     
 }
@@ -32,6 +34,20 @@
 
 
 - (IBAction)connectToDatabase:(id)sender {
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://ec2-54-68-112-35.us-west-2.compute.amazonaws.com/SE_test.py"]
+                                                          cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData
+                                                      timeoutInterval:10];
+    
+    [request setHTTPMethod: @"GET"];
+    
+    NSError *requestError;
+    NSURLResponse *urlResponse = nil;
+    
+    
+    NSData *response1 = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&requestError];
+    
+    NSLog(@"%@",[[NSString alloc] initWithData:response1 encoding:NSUTF8StringEncoding]);
     
     resultLabel.text = @"hello";
     
