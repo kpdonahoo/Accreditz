1389a5660691d0d6d4c76e5fe9d5705369f6afb4
kpdonahoo <kevindonahoo@Kevins-MacBook-Air.local>
2014-11-17 20:50:05 -0600

    First commit. Committing brand new project files.

---
+++ Accreditz/Accreditz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Accreditz
+//
+//  Created by Kevin Donahoo on 11/17/14.
+//  Copyright (c) 2014 Sharkbait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
