0b2941b8a777247dc6eb738fcd90679ef38441eb
Amanda Doyle <Amanda@Amandas-MacBook-Pro-2.local>
2014-11-22 12:47:05 -0600

    Saturday night stopping point

---
+++ Accreditz/Accreditz/ClassList.h
@@ -0,0 +1,13 @@
+//
+//  ClassList.h
+//  Accreditz
+//
+//  Created by Amanda Doyle on 11/21/14.
+//  Copyright (c) 2014 Sharkbait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClassList : UIViewController
+
+@end
--- Accreditz/Accreditz/ViewController.m
+++ Accreditz/Accreditz/ViewController.m
@@ -11,15 +11,14 @@
 @interface ViewController () <NSURLSessionTaskDelegate>
 @property (strong, nonatomic) IBOutlet UIView *connectButton;
 @property (weak, nonatomic) IBOutlet UILabel *resultLabel;
-@property (weak, nonatomic) IBOutlet UIButton *button;
 @property (strong, nonatomic) NSURLSession *session;
+#define SERVER_URL "http://ec2-54-68-112-35.us-west-2.compute.amazonaws.com:8000"
 
 @end
 
 //Demonstration
 @implementation ViewController
 @synthesize resultLabel;
-@synthesize button;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -50,13 +49,38 @@
     
     [request setHTTPMethod:@"POST"];
     
-    NSURLSessionDataTask *postTask = [self.session dataTaskWithRequest:request
-                                                     completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                                                         NSLog(@"%@",response);
-                                                     }];
-    [postTask resume];
+     NSURLSessionDataTask *postTask = [self.session dataTaskWithRequest:request
+     completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+     NSLog(@"%@",response);
+     }];
     
-    resultLabel.text = @"GOT A RESPONSE!";
+     [postTask resume];
+    
+    /*
+    // Create the request.
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://ec2-54-68-112-35.us-west-2.compute.amazonaws.com:8000/ConnectTest"]];
+    
+    // Specify that it will be a POST request
+    request.HTTPMethod = @"POST";
+    
+    // This is how we set header fields
+    [request setValue:@"application/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    // Convert your data and set your request's HTTPBody property
+    NSString *stringData = @"some data";
+    NSData *requestBodyData = [stringData dataUsingEncoding:NSUTF8StringEncoding];
+    request.HTTPBody = requestBodyData;
+    
+    // Create url connection and fire request
+    NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    
+*/
+    
+    NSString *url = @"http://ec2-54-68-112-35.us-west-2.compute.amazonaws.com:8000/Accreditz/tornado/ConnectTest";
+    
+    NSData *jsonDataString = [[NSString stringWithContentsOfURL:[NSURL URLWithString:url] encoding:NSUTF8StringEncoding error: nil] dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSLog(@"%@",jsonDataString);
     
 }
 
